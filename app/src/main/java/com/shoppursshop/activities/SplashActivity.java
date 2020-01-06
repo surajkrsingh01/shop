4911b6e5334e9fbd72e362dd1318fd48a4a44429
vipin.dhama8800@gmail.com <Vipin@8800>
2020-01-06 14:29:22 +0530

    Committed By Vipin Dhama

--- app/src/main/java/com/shoppursshop/activities/SplashActivity.java
+++ app/src/main/java/com/shoppursshop/activities/SplashActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Bundle;
@@ -122,7 +123,17 @@ public class SplashActivity extends NetworkBaseActivity {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
-            String IMEI = teleManager.getDeviceId();
+            String IMEI = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                IMEI =  teleManager.getImei();
+            } else {
+                IMEI = teleManager.getDeviceId();
+            }
+
+            if (IMEI == null || IMEI.isEmpty()) {
+                IMEI = android.os.Build.SERIAL;
+            }
+
             editor.putString(Constants.IMEI_NO,IMEI);
             editor.commit();
             Log.i(TAG,"Mac id "+IMEI);
